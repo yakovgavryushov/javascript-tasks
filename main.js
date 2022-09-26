5693d5123b70275b6e0f34aac5d127ef6a998bda
yakovgavryushov <95687203+yakovgavryushov@users.noreply.github.com>
2022-09-26 10:43:36 +0300

    added debugging through console

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 
-function task2(){
-    let enter = +prompt('Enter a number:','');
+function task2(num){
+    let enter = num;
     let i = enter;
     let arr = [];
-    
+
+    if(typeof num == "number"){
     do{ if(i > 1){      // if its more than 1 -- runs as usual, then 3 is 
         let impNum = ((i*2)-1);
         arr.push(impNum);// the lowest number
@@ -12,50 +13,72 @@ function task2(){
     else if(!arr.includes(0)) arr.push(0);   // if it drops lower than 1 --> push(0)
     else return console.log(String(arr));
 } while(i >= 0);
+    } else {
+        enter = +prompt('Enter a number:','');
+        task2(enter)
+    }
 }
 
-function task3(){
-let enter = +prompt('Enter a number:', '');
+
+function task3(num){
+let enter = num;
 let i = enter;          // made for keeping the number that is added every time
 let arr = [];
+    if(typeof num == "number"){
     do{
         arr.push(i*i);     // it multiplies itself
         i+=enter;       // and increments by itself after the first loop
 
         
-    } while(i < 30);    
-    return console.log(String(arr));
-} 
+    } while(i < 30);
+    return console.log(String(arr));}
+    else {
+        enter = +prompt('Enter a number:', '');
+        task3(enter)
+    }
+}
 
-function task4(){
-    let enterStart = +prompt('Enter a start number','');
-    let enterLines = +prompt('Enter number of lines', '');
-    let enterJump = +prompt('Enter a jump', '');
+function task4(num1, num2, num3){
+
+    let enterStart = num1; // +prompt('Enter a start number','');
+    let enterLines = num2; // +prompt('Enter number of lines', '');
+    let enterJump = num3; // +prompt('Enter a jump', '');
 
     let lines = 0;
     let jump = enterJump;
-
     let arr = [];
-    arr.push(enterStart); // shows start number firstly as it is
-    enterStart++;
-    lines++;
-    console.log(String(arr));
 
-    for(let i = enterStart; lines < enterLines; i++){
-        let arr = [];
-        
-        for(i; arr.length <= jump; i++){ 
-            arr.push(i);        
-        }                           // makes a line with incrementing numbers.
+    if(typeof num1 == "number" || typeof num2 == "number" || typeof num3 == "number") {
 
-        if(arr[0] !== i){               
-            arr.push(i); arr.pop(); i--;
+        arr.push(enterStart); // shows start number firstly as it is
+        enterStart++;
+        lines++;
+        console.log(String(arr));
+
+        for (let i = enterStart; lines < enterLines; i++) {
+            let arr = [];
+
+            for (i; arr.length <= jump; i++) {
+                arr.push(i);
+            }                           // makes a line with incrementing numbers.
+
+            if (arr[0] !== i) {
+                arr.push(i);
+                arr.pop();
+                i--;
+            }
+            // because the for loop is omitting first value (probably because of i++ or sth else), it regulates itself with this if statement.
+            jump += enterJump;            // counts and adds a jump for new line.
+            lines++;                    // counts lines to end when needed.
+
+            console.log(String(arr));                 // shows the result.
         }
-                                        // because the for loop is omitting first value (probably because of i++ or sth else), it regulates itself with this if statement.
+    }
+    else {
 
-        jump+=enterJump;            // counts and adds a jump for new line.
-        lines++;                    // counts lines to end when needed.
-
-        console.log(String(arr));                 // shows the result.
+        enterStart = +prompt('Enter a number:', '');;
+        enterLines = +prompt('Enter number of lines', '');
+        enterJump = +prompt('Enter a jump', '');
+        task4(enterStart, enterLines, enterJump)
     }
 }
